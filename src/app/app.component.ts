85a8f6226e5c02700d80eb4b1afd5072e768e8f2
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2024-03-26 13:31:41 -0600

    si

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'registro', loadComponent:() => import('./registro/registro.component').then(m => m.RegistroComponent)},
+    { path: 'login', loadComponent:() => import('./login/login.component').then(m => m.LoginComponent)},
+    { path: 'verification', loadComponent:() => import('./verification/verification.component').then(m => m.VerificationComponent)},
+    { path: 'about', loadComponent:() => import('./about/about.component').then(m => m.AboutComponent)},
+    { path: 'home', loadComponent:() => import('./home/home.component').then(m => m.HomeComponent)},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: '**', loadComponent:() => import('./not-found/not-found.component').then(m => m.NotFoundComponent)}
+];
