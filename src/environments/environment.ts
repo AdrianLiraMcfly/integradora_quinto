d344997e43459a85993efba07ddabebf3dad79a4
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2024-04-17 11:44:08 -0600

    aaaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     UrlAuth:'http://18.227.105.11:8000/api/auth',
     UrlUsers:'http://18.227.105.11:8000/api/v1/users',
-    UrlPlants:'http://18.227.105.11:8000/v1/plants',
+    UrlPlants:'http://18.227.105.11:8000/api/v1/plants',
     UrlSensors:'http://18.227.105.11:8000/api/v1/sensors',
     UrlMongo:'http://18.227.105.11:8000/api/v1/websocket',
     production:true
