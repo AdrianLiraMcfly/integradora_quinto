31e0e3b69ff21095ab1dedf95f3292415aa22b13
FERNANDO <fgolmos10@gmail.com>
2024-04-17 23:32:30 -0600

    ay

---
+++ dist/integradora/angular-app/chunk-6TRNSDK3.js
@@ -0,0 +1,1 @@
+import{P as a}from"./chunk-6IZXC2FC.js";var i=(()=>{let t=class t{constructor(){this.message=""}setMessage(e){this.message=e}getMessage(){let e=this.message;return this.message="",e}clearMessage(){this.message=""}};t.\u0275fac=function(r){return new(r||t)},t.\u0275prov=a({token:t,factory:t.\u0275fac,providedIn:"root"});let s=t;return s})();export{i as a};
