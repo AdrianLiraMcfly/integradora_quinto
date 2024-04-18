b9d4f7faa3eba6d5f4a1ed58e573bf7a3f426aa3
AdrianLiraMcfly <jorge.liralopez11@gmail.com>
2024-04-18 11:49:59 -0600

    rol en offcanvas

--- src/app/navtwo/navtwo.component.ts
+++ src/app/navtwo/navtwo.component.ts
@@ -14,15 +14,16 @@ export class NavtwoComponent implements OnInit{
  
   rol_id = 0
   constructor (private auth:AuthService,private router:Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.getRol()
     this.router.events.subscribe((event)=>{
       if(event.constructor.name == 'NavigationEnd'){
         this.getRol()
       }
     })
-  }
-
-  ngOnInit(): void {
-    this.getRol()
   }
 
   getRol(){
